cc4185a788eebab44639ddaaa82ff924acee810a
Neta Yamin <netayamin@gmail.com>
2021-03-04 13:52:30 +0200

    Initial Commit

---
+++ youtube_mvvm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  youtube_mvvm
+//
+//  Created by neta yamin on 04/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
